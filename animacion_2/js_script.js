9ec02fa65358b144acc05198d24d658f9c64c4ef
tintinve <pedr0261@stud.kea.dk>
2017-10-03 20:50:07 +0200

    V0.6

--- animacion_2/js_script.js
+++ animacion_2/js_script.js
@@ -1,3 +1,4 @@
+//Animacion 1 VOLAR
 {
 let imagen = document.querySelector(".rocket");
 let inicia_button = document.querySelector(".start_button");
@@ -5,7 +6,8 @@ inicia_button.addEventListener('click', empieza_vuela);
 
 function empieza_vuela(){
 imagen.classList.toggle("animacion_vuela");
-}}   
+}} 
+//Animacion 2 VOLAR
 {
 let imagen = document.querySelector(".rocket");
 let rotacion_button = document.querySelector(".rotacion_button");
@@ -13,4 +15,13 @@ rotacion_button.addEventListener('click', empieza_rotar);
 
 function empieza_rotar(){
 imagen.classList.toggle("animacion_rotacion");
+}}   
+//Animacion 3 moveTo30
+{
+let imagen = document.querySelector(".rocket");
+let moveTo30_button = document.querySelector(".moveTo30_button");
+moveTo30_button.addEventListener('click', moveTo30);
+
+function moveTo30(){
+imagen.classList.toggle("animacion_moveTo30");
 }}   
